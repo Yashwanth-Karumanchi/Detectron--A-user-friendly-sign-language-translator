825466efc7fcc5675dc6a83569d15f0c3e097a3f
Yashwanth-Karumanchi <yashwanthkarumanchi@gmail.com>
2023-09-17 16:26:22 +0530

    Added python availability check

--- App/Modify Data.bat
+++ App/Modify Data.bat
@@ -1,2 +1,13 @@
+@echo off
+python -c "print('Python is available')"
+if errorlevel 1 goto :python_not_found
+
 @cd /d "%~dp0\..\gui codes"
 @python.exe create_gui.py %*
+goto :end
+
+:python_not_found
+echo Python is not installed on this system.
+pause
+
+:end
--- App/Train the Model.bat
+++ App/Train the Model.bat
@@ -1,3 +1,13 @@
-@echo on
-@cd /d "%~dp0\..\GUI Codes"
+@echo off
+python -c "print('Python is available')"
+if errorlevel 1 goto :python_not_found
+
+@cd /d "%~dp0\..\gui codes"
 @python.exe train_gui.py %*
+goto :end
+
+:python_not_found
+echo Python is not installed on this system.
+pause
+
+:end
